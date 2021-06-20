0d296e48fd4f26ffe38b9c27cee3c32f1c7329c1
brunompasini <brunompasini@gmail.com>
2021-06-20 14:27:40 -0300

    tarefa 4 feita

--- tarefa4/tarefa4.cpp
+++ tarefa4/tarefa4.cpp
@@ -1,41 +1,23 @@
+// tarefa4.cpp
+// Bruno Mucha Pasini nUSP 10845791
+// Pedro Henrique Gianjoppe dos Santos nUSP 10774055
 #include <cekeikon.h>
-int main()
-{
-    Mat_<FLT> a; //Matriz ponto flutuante (0 = preto e 1 = branco)
-    le(a, "quadrado.png");
-    Mat_<FLT> sx, sy, ox, oy;
-    Sobel(a, sx, -1, 1, 0, 3);
-    ox = sx / 4.0 + 0.5;
-    imp(ox, "ox.png");
-    Sobel(a, sy, -1, 0, 1, 3);
-    oy = sy / 4.0 + 0.5;
-    imp(oy, "oy.png");
+int main() {
+    Mat_<FLT> a; le(a,"quadrado.png");
+    Mat_<FLT> sx,sy,ox,oy;
+    Sobel(a,sx,-1,1,0,3); ox=sx/4.0+0.5; imp(ox,"ox.png");
+    Sobel(a,sy,-1,0,1,3); oy=sy/4.0+0.5; imp(oy,"oy.png");
 
+    imp(sx, "sx.png");imp(sy, "sy.png");
+    
     Mat_<FLT> todo_hor, hor_sup, hor_inf, todo_ver, ver_esq, ver_dir;
-    todo_hor = a.clone();
-    hor_sup  = a.clone();
-    hor_inf  = a.clone();
-    todo_ver = a.clone();
-    ver_esq  = a.clone();
-    ver_dir  = a.clone();
 
-    for (int l = 0; l < a.rows; l++) {
-        for (int c = 0; c < a.cols; c++) {
-            
-        }        
-    }
-    /*Complete o programa aqui utilizando matrizes sx e sy
-    Complete o programa, fazendo operações com as matrizes sx e sy, para obter as imagens com:
-     • Todas bordas horizontais: todo_hor.png 
-    • A borda horizontal superior: hor_sup.png
-    • A borda horizontal inferior: hor_inf.png
-    • Todas as bordas verticais: todo_ver.png
-    • A borda vertical esquerda: ver_esq.png
-    • A borda vertical direita: ver_dir.png. */
+    hor_sup = sy.clone(); imp(hor_sup, "hor_sup.png");
+    ver_esq = sx.clone(); imp(ver_esq, "ver_esq.png");
+
+    flip(hor_sup, hor_inf, 0); imp(hor_inf, "hor_inf.png"); // funcao flip do openCV
+    flip(ver_esq, ver_dir, 1); imp(ver_dir, "ver_dir.png"); // idem
+
+    bitwise_and(hor_sup, hor_inf, todo_hor); imp(todo_hor, "todo_hor.jpg"); // bitwise_and do openCV
+    bitwise_and(ver_esq, ver_dir, todo_ver); imp(todo_ver, "todo_ver.jpg"); // idem
 }
-
-/* toda_hor = aplicar módulo
-
-hor_sup = valor negativo de sy  => '0' (preto). Assim todos os valores de saída são positivos ou zero (só borda superior)
-
-hor_inf = valor positivo de sy  => '0' (preto). Assim todos os valores de saída são negativos ou zero (só borda inferior) */
